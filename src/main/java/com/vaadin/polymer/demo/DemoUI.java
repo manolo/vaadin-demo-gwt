252663aeabe73c30965397262acdbf9d10252247
Manolo Carrasco <manolo@apache.org>
2016-01-30 21:27:01 +0100

    Use GWT patched with Jetty-9.
    Patch Message sender
    For some reason we need to patch DemoUI in order to load valo
    Remove our version of JettyLauncher since GWT-JETTY9 includes it

--- src/main/java/com/vaadin/polymer/demo/DemoUI.java
+++ src/main/java/com/vaadin/polymer/demo/DemoUI.java
@@ -2,6 +2,7 @@ package com.vaadin.polymer.demo;
 
 import javax.servlet.annotation.WebServlet;
 
+import com.vaadin.annotations.StyleSheet;
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.annotations.Widgetset;
@@ -9,6 +10,8 @@ import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.CssLayout;
+import com.vaadin.ui.JavaScript;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
@@ -23,21 +26,29 @@ public class DemoUI extends UI {
 
     @Override
     protected void init(VaadinRequest vaadinRequest) {
+        // For some reason this is not performed correctly in the bootstrap
+        String js =
+                "l = document.createElement('link');" +
+                "l.rel = 'stylesheet';" +
+                "l.href = 'VAADIN/themes/valo/styles.css';" +
+                "document.body.appendChild(l);" +
+                "document.body.classList.add('valo')";
+        JavaScript.eval(js);
+
         final VerticalLayout layout = new VerticalLayout();
-        
         final TextField name = new TextField();
         name.setCaption("Type your name here:");
 
         Button button = new Button("Click Me");
         button.addClickListener( e -> {
-            layout.addComponent(new Label("Thanks " + name.getValue() 
+            layout.addComponent(new Label("Thanks " + name.getValue()
                     + ", it works!"));
         });
-        
+
         layout.addComponents(name, button);
         layout.setMargin(true);
         layout.setSpacing(true);
-        
+
         setContent(layout);
     }
 
