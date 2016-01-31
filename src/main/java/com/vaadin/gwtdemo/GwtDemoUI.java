e13d1551438bf9a4b2db91f9a964cb917949e9c6
Manolo Carrasco <manolo@apache.org>
2016-01-31 14:19:15 +0100

    Refactor demo names

--- src/main/java/com/vaadin/gwtdemo/GwtDemoUI.java
+++ src/main/java/com/vaadin/gwtdemo/GwtDemoUI.java
@@ -1,16 +1,13 @@
-package com.vaadin.polymer.demo;
+package com.vaadin.gwtdemo;
 
 import javax.servlet.annotation.WebServlet;
 
-import com.vaadin.annotations.StyleSheet;
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.annotations.Widgetset;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
 import com.vaadin.ui.Button;
-import com.vaadin.ui.Button.ClickEvent;
-import com.vaadin.ui.CssLayout;
 import com.vaadin.ui.JavaScript;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.TextField;
@@ -20,40 +17,45 @@ import com.vaadin.ui.VerticalLayout;
 /**
  *
  */
-@Theme("valo")
-@Widgetset("com.vaadin.polymer.demo.DemoWidgetset")
-public class DemoUI extends UI {
+@Theme(GwtDemoUI.THEME)
+@Widgetset("com.vaadin.gwtdemo.GwtDemoWidgetset")
+public class GwtDemoUI extends UI {
+    
+    public static final String THEME = "gwtdemo";
 
     @Override
     protected void init(VaadinRequest vaadinRequest) {
+        
         // For some reason this is not performed correctly in the bootstrap
         String js =
                 "l = document.createElement('link');" +
                 "l.rel = 'stylesheet';" +
-                "l.href = 'VAADIN/themes/valo/styles.css';" +
+                "l.href = 'VAADIN/themes/" + THEME + "/styles.css';" +
                 "document.body.appendChild(l);" +
-                "document.body.classList.add('valo')";
+                "document.body.classList.add('" + THEME + "')";
         JavaScript.eval(js);
-
+        
+        
         final VerticalLayout layout = new VerticalLayout();
+        
         final TextField name = new TextField();
         name.setCaption("Type your name here:");
 
         Button button = new Button("Click Me");
         button.addClickListener( e -> {
-            layout.addComponent(new Label("Thanks " + name.getValue()
+            layout.addComponent(new Label("Thanks " + name.getValue() 
                     + ", it works!"));
         });
-
+        
         layout.addComponents(name, button);
         layout.setMargin(true);
         layout.setSpacing(true);
-
+        
         setContent(layout);
     }
 
-    @WebServlet(urlPatterns = "/*", name = "DemoUIServlet", asyncSupported = true)
-    @VaadinServletConfiguration(ui = DemoUI.class, productionMode = false)
-    public static class DemoUIServlet extends VaadinServlet {
+    @WebServlet(urlPatterns = "/*", name = "GwtDemoUIServlet", asyncSupported = true)
+    @VaadinServletConfiguration(ui = GwtDemoUI.class, productionMode = false)
+    public static class GwtDemoUIServlet extends VaadinServlet {
     }
 }
